819557b0a004e6a886523bfc4300fb084fbd4a94
Emanuelle <emanuelle.flash@gmail.com>
2026-08-21 12:00:57 -0300

    Restaurando script da contagem regressiva

--- js/script.js
+++ js/script.js
@@ -49,3 +49,33 @@ if (form) {
         });
     });
 }
+
+// CONTAGEM REGRESSIVA
+// Defina a data do evento no formato: YYYY-MM-DDTHH:mm:ss (Ex: 2026-10-15T15:00:00)
+const dataEvento = new Date('2026-11-22T15:00:00').getTime();
+
+function atualizarContagem() {
+    const agora = new Date().getTime();
+    const diferenca = dataEvento - agora;
+
+    if (diferenca > 0) {
+        const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+        const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+        const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+        const elDias = document.getElementById('dias');
+        const elHoras = document.getElementById('horas');
+        const elMin = document.getElementById('minutos');
+        const elSeg = document.getElementById('segundos');
+
+        if (elDias) elDias.innerText = dias < 10 ? '0' + dias : dias;
+        if (elHoras) elHoras.innerText = horas < 10 ? '0' + horas : horas;
+        if (elMin) elMin.innerText = minutos < 10 ? '0' + minutos : minutos;
+        if (elSeg) elSeg.innerText = segundos < 10 ? '0' + segundos : segundos;
+    }
+}
+
+// Atualiza a cada 1 segundo
+setInterval(atualizarContagem, 1000);
+atualizarContagem();
